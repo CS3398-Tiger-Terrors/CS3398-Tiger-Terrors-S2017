b14beab7ca6f91d0de28c8a3cab206612554bb0c
DKlesel <dck35@txstate.edu>
2017-02-27 18:02:26 -0600

    organize this shtuff

--- Project Files/Calculator/Calculator.java
+++ Project Files/Calculator/Calculator.java
@@ -2,97 +2,107 @@ package Calculator;
 
 /**
  * Created by calumklesel on 2/26/17.
+ * v1.02
  */
 
 public class Calculator {
-
-    protected double alk;
-    protected double chl;
-    protected double c_acid;
-    protected double hard;
-    protected double ph;
-    protected int pool_volume;
+    // Member variables that start with curr are the current value of the attribute.
+    // Member variables that end with amount are the final amount to add to get to a desired range of the attribute.
+    private double currAlk;
+    private double alkAmount;
+    private double currChl; // chlorine measured in ppm
+    private double chlAmount;
+    private double currCyAcid; // Conditioner/Cyanuric Acid levels measured in ppm
+    private double cyAcidAmount;
+    private double currHard;
+    private double hardAmount;
+    private double currPh;
+    private double phAmount;
+    private int pool_volume; // volume measured in gallons
+    private int volumeFactor; // volumeFactor is dependent upon the pool volume
 
     // Default constructor takes no arguments and initializes all private member variables to 0.
     public Calculator(){
-        alk = 0;
-        chl = 0;
-        c_acid = 0;
-        hard = 0;
-        ph = 0;
+        currAlk = 0;
+        currChl = 0;
+        currCyAcid = 0;
+        currHard = 0;
+        currPh = 0;
         pool_volume = 0;
+        volumeFactor = 0;
     }
 
     // Constructor accepts arguments for the starting values of the pool and initializes all private member variables
     // to the called argument values.
-    public Calculator(double alk, double chl, double c_acid, double hard, double ph, int pool_volume) {
-        this.alk = alk;
-        this.chl = chl;
-        this.c_acid = c_acid;
-        this.hard = hard;
-        this.ph = ph;
+    public Calculator(double currAlk, double currChl, double currCyAcid, double currHard, double currPh, int pool_volume) {
+        this.currAlk = currAlk;
+        this.currChl = currChl;
+        this.currCyAcid = currCyAcid;
+        this.currHard = currHard;
+        this.currPh = currPh;
         this.pool_volume = pool_volume;
+        calc_volumeFactor();
     }
 
     // Used instead of the other constructors when the pool volume is not known
-    public Calculator(double alk, double chl, double c_acid, double hard, double ph,
+    public Calculator(double currAlk, double currChl, double currCyAcid, double currHard, double currPh,
                       int length, int width, int height, String shape)
-
     {
-        this.alk = alk;
-        this.chl = chl;
-        this.c_acid = c_acid;
-        this.hard = hard;
-        this.ph = ph;
+        this.currAlk = currAlk;
+        this.currChl = currChl;
+        this.currCyAcid = currCyAcid;
+        this.currHard = currHard;
+        this.currPh = currPh;
         this.pool_volume = calc_pool_volume(length, width, height, shape);
+        calc_volumeFactor();
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Getters and setters
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public double getAlk(){
-        return alk;
+    public double getCurrAlk(){
+        return currAlk;
     }
 
-    public double getC_acid() {
-        return c_acid;
+    public double getCurrCyAcid() {
+        return currCyAcid;
     }
 
-    public double getChl() {
-        return chl;
+    public double getCurrChl() {
+        return currChl;
     }
 
-    public double getHard() {
-        return hard;
+    public double getCurrHard() {
+        return currHard;
     }
 
-    public double getPh() {
-        return ph;
+    public double getCurrPh() {
+        return currPh;
     }
 
     public int getPool_volume() {
         return pool_volume;
     }
 
-    public void setAlk(double alk) {
-        this.alk = alk;
+    public void setCurrAlk(double currAlk) {
+        this.currAlk = currAlk;
     }
 
-    public void setC_acid(double c_acid) {
-        this.c_acid = c_acid;
+    public void setCurrCyAcid(double currCyAcid) {
+        this.currCyAcid = currCyAcid;
     }
 
-    public void setChl(double chl) {
-        this.chl = chl;
+    public void setCurrChl(double currChl) {
+        this.currChl = currChl;
     }
 
-    public void setHard(double hard) {
-        this.hard = hard;
+    public void setCurrHard(double currHard) {
+        this.currHard = currHard;
     }
 
-    public void setPh(double ph) {
-        this.ph = ph;
+    public void setCurrPh(double currPh) {
+        this.currPh = currPh;
     }
 
     public void setPool_volume(int pool_volume) {
@@ -108,25 +118,45 @@ public class Calculator {
     // needs to calculate a single a few chemical levels without having to make a whole calculator object.
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static double calc_alk(double alk, int pool_volume) {
+/*    public static double calc_alk(double currAlk, int pool_volume) {
         return 0; //FIXME CALCULATIONS DONE HERE
     }
 
-    public static double calc_chl(double chl, int pool_volume) {
+    public static double calc_alk() {
         return 0; //FIXME CALCULATIONS DONE HERE
     }
 
-    public static double calc_c_acid(double c_acid, int pool_volume) {
+    public static double calc_chl(double currChl, int pool_volume) {
         return 0; //FIXME CALCULATIONS DONE HERE
     }
 
-    public static double calc_hard(double hard, int pool_volume) {
+    public static double calc_chl() {
         return 0; //FIXME CALCULATIONS DONE HERE
     }
 
-    public static double calc_ph(double ph, int pool_volume) {
+    public static double calc_c_acid(double currCyAcid, int pool_volume) {
         return 0; //FIXME CALCULATIONS DONE HERE
     }
+
+    public static double calc_c_acid() {
+        return 0; //FIXME CALCULATIONS DONE HERE
+    }
+
+    public static double calc_hard(double currHard, int pool_volume) {
+        return 0; //FIXME CALCULATIONS DONE HERE
+    }
+
+    public static double calc_hard() {
+        return 0; //FIXME CALCULATIONS DONE HERE
+    }
+
+    public static double calc_ph(double currPh, int pool_volume) {
+        return 0; //FIXME CALCULATIONS DONE HERE
+    }
+
+    public static double calc_ph() {
+        return 0; //FIXME CALCULATIONS DONE HERE
+    }*/
 
     public static int calc_pool_volume(int length, int width, int height, String shape) {
         double multiplier = 0;
@@ -144,6 +174,14 @@ public class Calculator {
                 multiplier = 1;
         }
         return (int) (length * width * height * multiplier);
+    }
+
+    public static int calc_volumeFactor(int pool_volume) {
+        return (pool_volume / 10000);
+    }
+
+    private void calc_volumeFactor() {
+        volumeFactor = pool_volume / 10000;
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -166,7 +204,10 @@ public class Calculator {
         return 0; //FIXME CALCULATIONS DONE HERE
     }
 
+    // Calculate and return the desired amount of chemicals to add to gain the ideal pH.
     public double calc_ph() {
-        return 0; //FIXME CALCULATIONS DONE HERE
+        Ph phObject = new Ph(currPh, pool_volume);
+        phAmount = phObject.calc_ph();
+        return phAmount;
     }
 }
