7653b7d8105af717a8ccd399cac428157b5886d4
CodySpradling <c_s255@txstate.edu>
2017-03-01 04:50:46 -0600

    Changed calculator, chlorine, cyacid, and pH
    Added hardness.java, changed chlorine, ph, and calculator to take into
    consideration the different chemicals to adjust the same measures.
    Fleshed out the implementation of the different classes in the main
    Calculator.java

--- Project Files/Calculator/Chlorine.java
+++ Project Files/Calculator/Chlorine.java
@@ -7,50 +7,58 @@ package Calculator;
 public class Chlorine extends Calculator {
     private double currChl;
     private int poolVolume;
-    private double amtNeeded; // Amount needed in fl oz is multiplied the volumeFactor
+    //private double amtNeeded; // Amount needed in fl oz is multiplied the volumeFactor
+    private double diNeed;    // Amount of dichlor for adjustment
+    private double triNeed;   // Amount of trichlor for adjustment
+    private double liqNeed;  // Amount of Liquid Chlorine for adjustment
     private double volumeFactor; // volumeFactor is dependent upon the pool volume
-    private double totalAmount; // Total amount to add of the chemical to get the desired Chlorine
-    private boolean trichlor;
-    private boolean dichlor;
+    //private double totalAmount; // Total amount to add of the chemical to get the desired Chlorine
+    //private boolean trichlor;
+    //private boolean dichlor;  //commenting out the bools as the class can give all values on command
 
     public Chlorine() {
         currChl = 0;
         poolVolume = 0;
-        amtNeeded = 0;
+        //amtNeeded = 0;
         volumeFactor = 0;
-        totalAmount = 0;
-        trichlor = false;
-        dichlor = false;
+        diNeed = triNeed = liqNeed = 0;
+        //totalAmount = 0;
+        //trichlor = false;
+        //dichlor = false;
     }
 
     public Chlorine(double currChl, int poolVolume) {
         this.currChl = currChl;
         this.poolVolume = poolVolume;
-        amtNeeded = 0;
+        //amtNeeded = 0;
         volumeFactor = Calculator.calcVolumeFactor(poolVolume);
-        totalAmount = 0;
-        trichlor = false;
-        dichlor = false;
+        diNeed = triNeed = liqNeed = 0;
+        //trichlor = false;
+        //dichlor = false;
     }
+
 
     @Override
-    public double calcChlorine() {
-        // FIXME - dependent upone the amtNeeded. multiply the amtNeeded by volumeFactor and set it to totalAmount.
-        return totalAmount;
-    }
-
-    private void calcAmtNeeded() {
+    private void calcChlorine() {
         if(currChl < 3) {
-            // FIXME - flag the bool value for which chemical to use and set amtNeeded to the amtNeeded factor
-            // Unsure of which chemical to use because both increase chlorine levels. Need some guidance here by Cody.
-        } else if(currChl > 3) {
-            // FIXME - flag the bool value for which chemical to use and set amtNeeded to the amtNeeded factor
+            diNeed = (2.15 * volumeFactor * (3-currChl));
+            triNeed = (1.5 * volumeFactor * (3-currChl));
+            liqNeed = (12.8 * volumeFactor * (3-currChl));
+
+        } else if(currChl >= 3) {
+            diNeed = triNeed = liqNeed = 0;
         }
     }
 
-    public boolean getTrichlor() { return trichlor; }
+    //public boolean getTrichlor() { return trichlor; }
 
-    public boolean getDichlor() { return dichlor; }
+    //public boolean getDichlor() { return dichlor; }
+
+    public double getDiNeed() { return diNeed; }
+
+    public double getTriNeed() { return triNeed; }
+
+    public double getLiqNeed() { return liqNeed; }
 
     @Override
     public double getCurrChl() { return currChl; }
