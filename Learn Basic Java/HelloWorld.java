0a1d4d0a25bb3f4ea9a4d2913bf6912fc3f29a60
Leo <lss70@txstate.edu>
2017-04-06 15:31:03 -0500

    Research Materials up to 4/6/17
    -FavoriteToys
    -Learn Basic Java
    -Lesson01_Favorite-Toys

---
+++ Learn Basic Java/HelloWorld.java
@@ -0,0 +1,11 @@
+// Java code for printing Hello world!
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    //write code in here that you want to execute
+
+        System.out.println("Hello Java!");
+    }
+
+}
